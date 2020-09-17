e3c063e1aca8c0e0a9d1b8a0bc00774ed27cac10
themoonbunny <themoonbunnys@gmail.com>
2020-09-17 22:15:23 +0200

    hi

---
+++ js.js
@@ -0,0 +1,7 @@
+var foto;
+window.onload = function(){
+    foto = new Foto();
+}
+function selectImage(){
+    document.getElementById("foto-file").onclick;
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+//rectangle
+ctx.fillRect(20, 20, 150, 100);
